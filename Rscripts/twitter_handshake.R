c3ffe865261fdd3d680c9812a0b79c45a25cf2eb
Daniel Salgado <elinda.999@gmail.com>
2015-08-01 23:16:43 -0500

    Delete twitter_handshake.R

--- Rscripts/twitter_handshake.R
+++
@@ -1,9 +0,0 @@
-## Handshake with twitter ##
-twitter_handshake <- function(){
-  consumer_key <- "ujZCTLbelg1gtoaZBtbx6VvRu"
-  consumer_secret <- "UZbeeW8TDRgBQ5qgXxCm8Y4wYYZj3YL5RSArpD8UIMOmEsiWhy"
-  access_token <- "19152060-bmFjZ1aBnUz52mhrbJzAnQepNNC5uMZSA5e3H82BS"
-  access_secret <- "G2LzCZBPHZY6p2Ca9MlXXeuMNaR17Dc2pm3oQiPtD6T5w"
-  setup_twitter_oauth(consumer_key, consumer_secret,
-                      access_token, access_secret)  
-}
